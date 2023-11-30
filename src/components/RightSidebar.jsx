0c0ba87db1347f3c2f9674cca968cdd9c4f1112a
HassanAllawati21 <80989313+HassanAllawati21@users.noreply.github.com>
2023-11-30 13:14:51 -0800

    calendar not functional

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -1,81 +1,52 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const RightSidebar = ({ selectedUser, projects }) => {
-  const [currentDate, setCurrentDate] = useState(new Date());
+  // Assuming projects is an array of project objects with a dueDate property
 
-  const getDaysInMonth = (date) => {
+  // Function to get the month and year of a project's due date
+  const getMonthYear = (dueDate) => {
+    const date = new Date(dueDate);
+    const month = date.toLocaleString('en-US', { month: 'long' });
     const year = date.getFullYear();
-    const month = date.getMonth();
-    return new Date(year, month + 1, 0).getDate();
+    return `${month} ${year}`;
   };
 
-  const getFirstDayOfMonth = (date) => {
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    return new Date(year, month, 1).getDay();
+  // Function to get the day of the month for a project's due date
+  const getDay = (dueDate) => {
+    const date = new Date(dueDate);
+    return date.getDate();
   };
 
-  const buildCalendar = () => {
-    const daysInMonth = getDaysInMonth(currentDate);
-    const firstDayOfMonth = getFirstDayOfMonth(currentDate);
-
-    const calendar = [];
-
-    // Add blank spaces for the days before the first day of the month
-    for (let i = 0; i < firstDayOfMonth; i++) {
-      calendar.push(null);
-    }
-
-    // Add days with project due dates
-    for (let day = 1; day <= daysInMonth; day++) {
-      const isDueDate = projects.some((project) => {
-        const dueDate = new Date(project.dueDate);
-        return dueDate.getDate() === day;
-      });
-
-      calendar.push({ day, isDueDate });
-    }
-
-    return calendar;
+  // Function to check if a day has projects due
+  const hasProjectsDue = (day) => {
+    return projects.some((project) => getDay(project.dueDate) === day);
   };
-
-  const handlePrevMonth = () => {
-    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1));
-  };
-
-  const handleNextMonth = () => {
-    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1));
-  };
-
-  const monthOptions = [
-    'January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December'
-  ];
 
   return (
     <div className="right-sidebar">
-      {/* Month Navigation */}
-      <div>
-        <button onClick={handlePrevMonth}>&lt;</button>
-        {monthOptions[currentDate.getMonth()]}
-        <button onClick={handleNextMonth}>&gt;</button>
+      {/* Calendar Header */}
+      <div className="calendar-header">
+        <button>{"<"}</button>
+        <h3>{getMonthYear(new Date())}</h3>
+        <button>{">"}</button>
       </div>
 
-      {/* Calendar */}
-      <div className="calendar">
-        <div className="days-of-week">
-          <span>Sun</span>
-          <span>Mon</span>
-          <span>Tue</span>
-          <span>Wed</span>
-          <span>Thu</span>
-          <span>Fri</span>
-          <span>Sat</span>
-        </div>
+      {/* Calendar Grid */}
+      <div className="calendar-grid">
+        {/* Days of the week */}
+        {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map((day) => (
+          <div key={day} className="day-of-week">
+            {day}
+          </div>
+        ))}
 
-        {buildCalendar().map((day, index) => (
-          <div key={index} className={`calendar-day ${day && day.isDueDate ? 'due-date' : ''}`}>
-            {day && day.day}
+        {/* Calendar days */}
+        {Array.from({ length: 31 }, (_, index) => index + 1).map((day) => (
+          <div
+            key={day}
+            className={`calendar-day ${hasProjectsDue(day) ? 'highlighted' : ''}`}
+          >
+            {day}
           </div>
         ))}
       </div>
